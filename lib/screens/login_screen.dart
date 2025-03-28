67262eb7417d8e1b8912775b6257900748516d70
jose <elder.ortiz2318@gmail.com>
2025-03-27 20:45:28 -0500

    initial commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,8 +8,32 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: AuthBackground()
-   );
+    return   Scaffold(
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              const SizedBox( height: 250 ),
+
+             CardContainer(
+              child: Column(
+                children: [
+
+                  const SizedBox( height: 10 ),
+                  Text('Login', style: Theme.of(context).textTheme.headlineMedium ),
+                  const SizedBox( height: 30 ),
+
+                  const Text('Formulario')
+
+                ],
+              )
+             ),
+
+            ],
+          ),
+        )
+      )
+    );
   }
 }
--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
 
 class AuthBackground extends StatelessWidget {
-  const AuthBackground({super.key});
+
+  final Widget child;
+
+    const AuthBackground({
+      super.key, 
+      required this.child});
+
 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
      // color: Colors.red,
       width: double.infinity,
       height: double.infinity,
       child: Stack(
         children: [
 
-         _PurpleBox()
+         _PurpleBox(),
+
+         _HeaderIcon(),
+
+         child,
 
         ],
+      ),
+    );
+  }
+}
+
+class _HeaderIcon extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+       width: double.infinity,
+       margin: const EdgeInsets.only( top: 30 ),
+       child: const Icon( Icons.person_pin, color: Colors.white, size: 100 ),
       ),
     );
   }
